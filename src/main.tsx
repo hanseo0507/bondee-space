42da9e8a02619b0e61ffa0f629fe368833d43a95
HanSu Lee <i.hansu.lee@gmail.com>
2023-02-12 01:12:21 +0900

    feat(styles): add global-style

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Global } from '@emotion/react';
+
 import { App } from './App';
+import { globalStyle } from './styles';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>
 );
---
+++ src/styles/global.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const globalStyle = css`
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+    background-color: #e3e3e2;
+  }
+`;
